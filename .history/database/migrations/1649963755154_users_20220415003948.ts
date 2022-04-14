ad008ab3dc5e6285fe4d6f2be7087aab84a2d782
mbell20 <mbell9260@gmail.com>
2022-04-15 01:12:12 +0500

    user table created

---
+++ .history/database/migrations/1649963755154_users_20220415003948.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Users extends BaseSchema {
+  protected tableName = 'users'
+
+  public async up () {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+
+
+      table.string('name', 55);
+      table.string('email', 55);
+      table.string('password', 55);
+      table.string('image', 55);
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down () {
+    this.schema.dropTable(this.tableName)
+  }
+}
